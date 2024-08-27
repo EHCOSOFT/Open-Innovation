7288749d492ee9b5aa7c426b2f8577960ca90933
ksw <ksw@ehcosoft.com>
2024-08-27 12:15:15 +0900

    20240827 수정사항

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -29,22 +29,22 @@ $(document).ready(function () {
     $('.navbar ul li a').on('click', function (e) {
         e.preventDefault();
 
-    // Remove active class from all li elements
-    $('.navbar ul li').removeClass('active');
+        // Remove active class from all li elements
+        $('.navbar ul li').removeClass('active');
 
-    // Remove active class from all i elements
-    $('.navbar ul li i').removeClass('active');
+        // Remove active class from all i elements
+        $('.navbar ul li i').removeClass('active');
 
-    // Add active class to the parent li of the clicked a
-    $(this).parent('li').addClass('active');
-    $(this).children('i').addClass('active');
+        // Add active class to the parent li of the clicked a
+        $(this).parent('li').addClass('active');
+        $(this).children('i').addClass('active');
 
-    // Remove active class from all .menu-layer elements
-    $('.menu-layer').removeClass('active');
+        // Remove active class from all .menu-layer elements
+        $('.menu-layer').removeClass('active');
 
-    // Add active class to the connected target
-    const target = $(this).data('target'); // Get target id from data-target property
-    $(target).addClass('active');
+        // Add active class to the connected target
+        const target = $(this).data('target'); // Get target id from data-target property
+        $(target).addClass('active');
     });
 
     // 모달 열기 버튼 클릭 이벤트
@@ -67,8 +67,38 @@ $(document).ready(function () {
         e.stopPropagation();
     });
 
-    $(".btn-zone").click(function() {
+    $(".btn-zone").click(function () {
         $(this).toggleClass("active");
     })
-});
 
+    // 버튼 클릭 이벤트 리스너 추가
+    $('#btn-lock').on('click', function () {
+        // active 클래스 토글 (추가/제거)
+        $(this).toggleClass('active');
+
+        // 현재 텍스트를 확인하고 "잠금"와 "해제" 사이를 토글
+        if ($(this).text() === '잠금') {
+            $(this).text('해제');
+        } else {
+            $(this).text('잠금');
+        }
+    });
+
+    // 위치 추가
+    $('.add-location').on('click', function(e) {
+        e.preventDefault(); // 기본 동작 방지 (예: 페이지 이동 방지)
+
+        // 추가할 새로운 HTML 요소
+        var newInputArea = `
+            <div class="input-area horizontal">
+                <input type="text" placeholder="장소명">
+                <input type="text" placeholder="위도">
+                <input type="text" placeholder="경도">
+                <button type="button" class="btn btn-sm btn-gray btn-add-location">저장</button>
+            </div>
+        `;
+
+        // .add-location 앞에 새로운 요소 삽입
+        $(this).before(newInputArea);
+    });
+});
